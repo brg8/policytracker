54445a3136cb6b21d70c9d5d4f3f2653644770d6
Benjamin Godlove <ben@pluslabs.org>
2021-01-06 18:06:24 -0500

    Tidy up and overwrite html file

--- scrapers/fb.sh
+++ scrapers/fb.sh
@@ -2,14 +2,18 @@
 
 POLICY_DIR=policies
 THIS_FILE_NAME=$(basename "$0")
+POLICY_FILE=$POLICY_DIR/fb.html
+POLICY_TMP_FILE=$POLICY_DIR/fb_temp.html
 
 if [ $(basename $(pwd)) != "tospp" ]; then
   echo "Must run this script from tospp folder"
   exit 1
 fi
 
-curl "https://www.facebook.com/policy.php" > $POLICY_DIR/fb.html
-tidy $POLICY_DIR/fb.html
+curl "https://www.facebook.com/policy.php" > $POLICY_FILE
+tidy $POLICY_FILE > $POLICY_TMP_FILE
+cp $POLICY_TMP_FILE $POLICY_FILE
+rm $POLICY_TMP_FILE
 
 if [ $(git branch --show-current) = "main" ]; then
   git add $POLICY_DIR/*
