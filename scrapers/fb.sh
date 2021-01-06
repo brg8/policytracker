1922f6351c1cb824502e1d70994140ac3cad8011
Benjamin Godlove <ben@pluslabs.org>
2021-01-06 17:52:37 -0500

    Tidy up the html for readability

--- scrapers/fb.sh
+++ scrapers/fb.sh
@@ -3,13 +3,18 @@
 POLICY_DIR=policies
 THIS_FILE_NAME=$(basename "$0")
 
-# echo $(basename $(pwd))
 if [ $(basename $(pwd)) != "tospp" ]; then
   echo "Must run this script from tospp folder"
   exit 1
 fi
 
 curl "https://www.facebook.com/policy.php" > $POLICY_DIR/fb.html
-git add $POLICY_DIR/*
-git commit -m "Executed file $THIS_FILE_NAME"
+tidy $POLICY_DIR/fb.html
+
+if [ $(git branch --show-current) = "main" ]; then
+  git add $POLICY_DIR/*
+  git commit -m "Executed file $THIS_FILE_NAME"
+  git push origin main
+fi
+
 exit 0
